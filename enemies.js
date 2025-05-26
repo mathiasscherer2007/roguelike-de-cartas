148a345dbf0a6a65cde365a3ae57c966dd162aaa
Xerão <mathischerer@gmail.com>
2025-05-25 21:56:06 -0300

    Beginning of the wave system
    Started developing the wave system as well as enemy types and things of the sort.
    
    Enemy types are different from weapons, in that their type (or race, per se) is defined by their position on a list, rather than expressed in the class itself.

--- enemies.js
+++ enemies.js
@@ -12,5 +12,34 @@ class Enemy{
 const garcia = new Enemy(1, "Hermano Pera", 3, 5, "Images/Enemies/pera.png", "");
 const goblin = new Enemy(2, "Goblin", 1, 1, "Images/Enemies/goblin.png", "");
 
+/*
 
-export const enemyList = [garcia, goblin];
+    Enemies are separated by type as well,
+    but this separation occurs in the list itself.
+
+    index 0 = goblin;
+    index 1 = human;
+    index 2 = animal;
+    index 3 = demon;
+    index 4 = misc;
+    index 5 = everyone;
+
+    Inside these lists, index 0 represents a list of bosses.
+    Stupid ass way of organizing this,
+    but whatever, maybe some day I'll change it.
+
+    enemyList = [type1[[boss, boss], enemy1, enemy2], 
+                 type2[[boss, boss], enemy1, enemy2], 
+                 type3[[boss, boss], enemy1, enemy2]]
+
+*/
+
+const enemyList =  [[[], goblin],
+                    [[], ],
+                    [[], ],
+                    [[], ],
+                    [[], garcia],
+                    [[], goblin, garcia]];
+
+
+export {enemyList};
--- main.js
+++ main.js
@@ -11,20 +11,68 @@ const divWEAPONS = document.querySelector(".weapons");
 const divBACKPACK = document.querySelector(".backpack");
 const divHEALTH = document.querySelector(".health");
 
-// let manoJohn = new CardEnemy(enemyList[0].name, enemyList[0].damage, enemyList[0].health, enemyList[0].image, enemyList[0].ability);
-// divPLACEMENT.appendChild(manoJohn.element);
-let goblin = new CardEnemy(enemyList[1].name, enemyList[1].damage, enemyList[1].health, enemyList[1].image, enemyList[1].ability);
-divPLACEMENT.appendChild(goblin.element);
-let goblin1 = new CardEnemy(enemyList[1].name, enemyList[1].damage, enemyList[1].health, enemyList[1].image, enemyList[1].ability);
-divPLACEMENT.appendChild(goblin1.element);
-let goblin2 = new CardEnemy(enemyList[1].name, enemyList[1].damage, enemyList[1].health, enemyList[1].image, enemyList[1].ability);
-divPLACEMENT.appendChild(goblin2.element);
-let goblin3 = new CardEnemy(enemyList[1].name, enemyList[1].damage, enemyList[1].health, enemyList[1].image, enemyList[1].ability);
-divPLACEMENT.appendChild(goblin3.element);
+let eventCounter = 0; // Number of events the player has passed.
+let turnCounter = 0;
 
-let arma = new CardWeapon(weaponsList[0].name, 4, weaponsList[0].health, weaponsList[0].image, weaponsList[0].ability, weaponsList[0].type);
-divWEAPONS.appendChild(arma.element);
-weaponsInHand.push(arma);
-let arma2 = new CardWeapon(weaponsList[2].name, weaponsList[2].damage, weaponsList[2].health, weaponsList[2].image, weaponsList[2].ability, weaponsList[2].type);
-divWEAPONS.appendChild(arma2.element);
-weaponsInHand.push(arma2);
+// // let manoJohn = new CardEnemy(enemyList[0].name, enemyList[0].damage, enemyList[0].health, enemyList[0].image, enemyList[0].ability);
+// // divPLACEMENT.appendChild(manoJohn.element);
+// let goblin = new CardEnemy(enemyList[1].name, enemyList[1].damage, enemyList[1].health, enemyList[1].image, enemyList[1].ability);
+// divPLACEMENT.appendChild(goblin.element);
+// let goblin1 = new CardEnemy(enemyList[1].name, enemyList[1].damage, enemyList[1].health, enemyList[1].image, enemyList[1].ability);
+// divPLACEMENT.appendChild(goblin1.element);
+// let goblin2 = new CardEnemy(enemyList[1].name, enemyList[1].damage, enemyList[1].health, enemyList[1].image, enemyList[1].ability);
+// divPLACEMENT.appendChild(goblin2.element);
+// let goblin3 = new CardEnemy(enemyList[1].name, enemyList[1].damage, enemyList[1].health, enemyList[1].image, enemyList[1].ability);
+// divPLACEMENT.appendChild(goblin3.element);
+
+// let arma = new CardWeapon(weaponsList[0].name, 4, weaponsList[0].health, weaponsList[0].image, weaponsList[0].ability, weaponsList[0].type);
+// divWEAPONS.appendChild(arma.element);
+// weaponsInHand.push(arma);
+// let arma2 = new CardWeapon(weaponsList[2].name, weaponsList[2].damage, weaponsList[2].health, weaponsList[2].image, weaponsList[2].ability, weaponsList[2].type);
+// divWEAPONS.appendChild(arma2.element);
+// weaponsInHand.push(arma2);
+
+generateEvent(eventCounter);
+
+function generateEvent(eventCounter){
+    let eventType = "";
+
+    if (eventCounter%2 === 0) {
+        eventType = "battle";
+
+        let hordeType = Math.floor(Math.random() * enemyList.length);
+
+        switch (hordeType){
+            case 0:                         // goblins
+                console.log("goblins");
+
+                const selectedEnemyList = enemyList[hordeType];
+                
+
+                break;
+            case 1:                         // humans
+                console.log("humans");
+                break;
+            case 2:                         // animals
+                console.log("animals");
+                break;
+            case 3:                         // demons
+                console.log("demons");
+                break;
+            case 4:                         // misc
+                console.log("misc");
+                break;
+            case 5:                         // everyone
+                console.log("whoopsie daisy! everyone is here!");
+                break;
+            default:
+                console.log(hordeType);
+                break
+        }
+
+    } else {
+        eventType = "shop";
+    }
+
+    console.log(eventType);
+}
